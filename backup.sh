659558f4111ee90bb896e116a70a8afbeb14a235
alysha10 <andy942@guru.smk.belajar.id>
2026-02-09 21:12:26 +0800

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Konfigurasi
+DB_NAME="moodle2"
+DB_USER="root"
+BACKUP_DIR="/home"
+DATE=$(date +"%Y-%m-%d_%H-%M")
+
+# Nama file backup
+BACKUP_FILE="$BACKUP_DIR/${DB_NAME}_backup_$DATE.sql"
+
+# Proses backup
+mysqldump -u $DB_USER $DB_NAME > $BACKUP_FILE
+
+# Optional: hapus backup lebih dari 7 hari
+find $BACKUP_DIR -name "${DB_NAME}_backup_*.sql" -type f -mtime +7 -delete
